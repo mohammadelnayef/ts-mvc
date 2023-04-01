62be26bdbdbc06c6ef97becb3dfab0cd3ec98bb1
Mohammad <mohammad.elnayef@emag.ro>
2023-04-01 15:24:46 +0300

    Added Jest

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -1,9 +1,9 @@
 import fetchData from '../models/example.model';
-import dataService from '../services/process-data.service';
+import processData from '../services/process-data.service';
 const path: string = 'https://jsonplaceholder.typicode.com/users';
 const getProcessedData = async (req: any, res: any) => {
     const apiData = await fetchData(path);
-    const result = dataService.processData(apiData);
+    const result = processData(apiData);
     res.json(result);
 }
 
--- src/services/process-data.service.ts
+++ src/services/process-data.service.ts
@@ -2,4 +2,4 @@ const processData = (data: any) => {
     return `Hi i am ${data[0].name} and i got fetched from an api.`;
 }
 
-export default {processData}
+export default processData
